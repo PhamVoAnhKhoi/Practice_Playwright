bb38645d48995cbd71361f51318d96000a329596
intern.pvakhoi <pvakhoi-batch49@sdc.edu.vn>
2025-09-22 17:22:43 +0700

    UnhappyTest(Invalid Password & Empty Account)

--- src/test/java/utils/AccountData.java
+++ src/test/java/utils/AccountData.java
@@ -3,9 +3,24 @@ package utils;
 public class AccountData {
     public String userName;
     public String passWord;
+    public String invalidUserName;
+    public String invalidPassWord;
+    public String invalidStatus;
+    public String emptyUserName;
+    public String emptyPassWord;
+    public String emptyStatus;
+
 
     public AccountData(){
         this.userName = "Admin";
         this.passWord = "admin123";
+        this.invalidUserName = "InternTMA";
+        this.invalidPassWord = "Abc@123";
+        this.invalidStatus = "Invalid credentials";
+        this.emptyUserName = "";
+        this.emptyPassWord = "";
+        this.emptyStatus = "Required";
     }
+
+
 }
